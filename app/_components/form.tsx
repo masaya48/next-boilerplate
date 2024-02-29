554e92cb28572c188e6dac4d87c4c764ef0e6743
masaya48 <msya0408@gmail.com>
2024-03-01 00:30:36 +0900

    knip

---
+++ app/_components/form.tsx
@@ -0,0 +1,20 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const schema = z.object({
+  name: z.string(),
+});
+
+export const Form: FC = () => {
+  const { register } = useForm({
+    resolver: zodResolver(schema),
+  });
+  return (
+    <form>
+      <input type="text" {...register('name')} />
+      <button>submit</button>
+    </form>
+  );
+};
---
+++ knip.js
@@ -0,0 +1,22 @@
+/** @type {import('knip').KnipConfig} */
+export default {
+  entry: ['.scaffdog/*'],
+  project: ['app/**/*.(ts|tsx)'],
+  ignoreDependencies: [
+    // tailwindcss dependencies
+    'postcss',
+
+    // eslint
+    '@typescript-eslint/eslint-plugin',
+    'eslint-import-resolver-typescript',
+    'eslint-plugin-import',
+    'eslint-plugin-import-access',
+    'eslint-plugin-storybook',
+    'eslint-plugin-testing-library',
+    'eslint-plugin-vitest',
+
+    // @types
+    '@types/npm',
+  ],
+  ignoreExportsUsedInFile: true,
+};
